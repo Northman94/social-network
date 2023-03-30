e6ad51c7125769a00b6d0770442e053dfc87c352
Northman94 <Zhandarm2000@gmail.com>
2023-03-30 22:33:25 +0300

    app factory method realisation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,11 +3,15 @@ from flask_wtf import FlaskForm
 from wtforms import (
     StringField,
     validators,
-    PasswordField
+    PasswordField,
+    BooleanField,
+    SubmitField
 )
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[validators.DataRequired() ])
     password = PasswordField("Password", validators=[validators.DataRequired(), validators.Length(min=6)])
+    remember = BooleanField("Remember")
+    submit = SubmitField("Log In")
 
 
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,11 @@
-from . import bp
-from flask import render_template
+from . import bp  #from app.auth import bp
+from flask import render_template, redirect, url_for
+from .forms import LoginForm
 
-@bp.route("/")
-def index():
-    return "Hello from auth"
 
-@bp.route("/login")
+@bp.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("auth/login.html", form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for("index"))
+    return render_template("auth/login.html", form=form)
